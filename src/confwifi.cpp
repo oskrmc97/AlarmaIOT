f7594639912cfbc5140b3ad0a9103c32057d6958
Oscar Melo <oskr975@gmail.com>
2023-02-21 18:10:09 -0500

    Error clientloop fixed and callback fixed too

--- src/confwifi.cpp
+++ src/confwifi.cpp
@@ -3,10 +3,7 @@
 #include "PubSubClient.h"
 
 class confwifi{
-     
-    const char* mqttUser = "racso";
-    const char* mqttPassword = "bimborico22D";
-    
+        
     const char* ssid = "Nasus_p1";
     const char* password = "oscar1234@";
 
@@ -22,41 +19,6 @@ class confwifi{
         }
         Serial.println("\n Connected");
         Serial.println("SSID: Nasus_p1 ");
-    }
-
-    public:
-    void mqttconnect(const char* mqttServer,const int mqttPort){
-        WiFiClient espclient;   
-        PubSubClient client(espclient);
-        client.setServer(mqttServer,mqttPort);
-        while(!client.connected()){
-            Serial.println("Connecting to MQTT...");
-            delay(2000);
-            if(client.connect("espprincipal",mqttUser,mqttPassword)){
-                Serial.println("connected");
-                const char* message = "Hello World i am connected to MQTT";
-                int length = strlen(message);
-                boolean retained = true;
-                client.publish("prueba1",(byte*)message,length,retained);
-                Serial.println("mensaje sent");
-                delay(3000);
-            }
-        }
-    }
-
-    public:
-    void mqtttopic(PubSubClient client){
-        client.subscribe("prueba1");
-        client.setCallback([](char* topic, byte* payload, unsigned int length) {
-            Serial.print("Message arrived in topic: ");
-            Serial.println(topic);
-            Serial.print("Message:");
-            for (int i = 0; i < length; i++) {
-                Serial.print((char)payload[i]);
-            }
-            Serial.println();
-            Serial.println("-----------------------");
-        });
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,56 @@
 #include <confwifi.cpp>
 
 
+
 confwifi wifi;
+WiFiClient espclient;
+PubSubClient client(espclient);
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void mqttconnect(const char* mqttServer,const int mqttPort, WiFiClient espclient, const char* mqttUser,const char* mqttPassword){  
+  client.setServer(mqttServer,mqttPort);
+  while(!client.connected()){
+      Serial.println("Connecting to MQTT...");
+      delay(2000);
+      if(client.connect("espprincipal",mqttUser,mqttPassword)){
+          Serial.println("connected");
+          const char* message = "Hello World i am connected to MQTT";
+          int length = strlen(message);
+          boolean retained = true;
+          client.publish("prueba2",(byte*)message,length,retained);
+          Serial.println("mensaje sent");
+          if(client.subscribe("prueba1"))
+              Serial.println("Suscribed to topic!");
+          else
+              Serial.println("Error to subscribe!");
+          delay(2000);
+      }
+      else{
+          Serial.println("Error connecting to MQTT");
+          Serial.println(client.state());
+          delay(2000);
+      }
+  }
+}
+
+
 void setup() {
   Serial.begin(115200);
   wifi.wificonect();
-  wifi.mqttconnect("broker.mqttdashboard.com",1883);
+  mqttconnect("broker.mqttdashboard.com",1883, espclient,"racso","bimborico22D");
+  client.setCallback(callback);
 }
+
 void loop() {
+  client.loop();
 }
