42d16aa56c468269006cbe179436eedf48e0084f
Oscar Melo <oskr975@gmail.com>
2023-02-27 15:04:39 -0500

    Activa el AP, falta activar con boton

--- src/confwifi.cpp
+++ src/confwifi.cpp
@@ -1,16 +1,65 @@
-#include "WiFi.h"
-#include "mqtt_client.h"
-#include "PubSubClient.h"
+#ifdef ESP32
+#include <esp_wifi.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+#define ESP_getChipId()   ((uint32_t)ESP.getEfuseMac())
+
+#define LED_ON      HIGH
+#define LED_OFF     LOW
+#else
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+
+#define ESP_getChipId()   (ESP.getChipId())
+
+#define LED_ON      LOW
+#define LED_OFF     HIGH
+#endif
+
 
 class confwifi{
-        
-    const char* ssid = "Nasus_p1";
-    const char* password = "oscar1234@";
+
+    
+    public:
+    void heartBeatPrint(void){
+    static int num = 1;
+
+    if (WiFi.status() == WL_CONNECTED)
+        Serial.print("H");        // H means connected to WiFi
+    else
+        Serial.print("F");        // F means not connected to WiFi
+
+    if (num == 80)
+    {
+        Serial.println();
+        num = 1;
+    }
+    else if (num++ % 10 == 0)
+    {
+        Serial.print(" ");
+    }
+    }
+    public:
+    void check_status()
+    {
+    static ulong checkstatus_timeout = 0;
+
+    #define HEARTBEAT_INTERVAL    10000L
+    // Print hearbeat every HEARTBEAT_INTERVAL (10) seconds.
+    if ((millis() > checkstatus_timeout) || (checkstatus_timeout == 0))
+    {
+        heartBeatPrint();
+        checkstatus_timeout = millis() + HEARTBEAT_INTERVAL;
+    }
+    }
 
     public:
     void wificonect(){
 
-        WiFi.begin(ssid, password);
+        // WiFi.begin(ssid, password);
         Serial.println("\nconnecting....");
 
         while(WiFi.status() !=  WL_CONNECTED){
@@ -18,7 +67,7 @@ class confwifi{
             delay(1000);
         }
         Serial.println("\n Connected");
-        Serial.println("SSID: Nasus_p1 ");
+        Serial.println("SSID: Nasus_p1");
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,51 @@
 #include <Arduino.h>
 #include <confwifi.cpp>
+#include <ESP_WiFiManager.h> 
+#include "mqtt_client.h"
+#include "PubSubClient.h"
 
 
 
 confwifi wifi;
 WiFiClient espclient;
+String Router_SSID;
+String Router_Pass;
 PubSubClient client(espclient);
+
 int activar = 50;
 void callback(char* topic, byte* payload, unsigned int length) {
    activar = (int)payload[0];
   }
+
+void autoconnectap(){
+  ESP_WiFiManager ESP_wifiManager("AutoConnectAP");
+  ESP_wifiManager.setDebugOutput(true);
+  ESP_wifiManager.setAPStaticIPConfig(IPAddress(192, 168, 0, 120), IPAddress(192, 168, 0, 1), IPAddress(255, 255, 255, 0));
+  ESP_wifiManager.setMinimumSignalQuality(-1);
+  // ESP_wifiManager.setSTAStaticIPConfig(IPAddress(192, 168, 2, 114), IPAddress(192, 168, 2, 1), IPAddress(255, 255, 255, 0),IPAddress(192, 168, 2, 1), IPAddress(8, 8, 8, 8));
+  Router_SSID = ESP_wifiManager.WiFi_SSID();
+  Router_Pass = ESP_wifiManager.WiFi_Pass();
+  Serial.println("Stored: SSID = " + Router_SSID + ", Pass = " + Router_Pass);
+
+  if (Router_SSID != "")
+      {
+          ESP_wifiManager.setConfigPortalTimeout(300); //If no access point name has been previously entered disable timeout.
+          Serial.println("Got stored Credentials. Timeout 60s");
+      }
+      else
+      {
+          Serial.println("No stored Credentials. No timeout");
+      }
+
+  String chipID = String(ESP_getChipId(), HEX);
+  chipID.toUpperCase();
+  String AP_SSID = "Alarma" + chipID;
+  String AP_PASS = "ESP_" + chipID;
+  ESP_wifiManager.resetSettings();
+  ESP_wifiManager.autoConnect(AP_SSID.c_str(), AP_PASS.c_str());
+  Serial.println("WiFi connected la ip es:" + WiFi.localIP().toString());
+}
+
 
 void mqttconnect(const char* mqttServer,const int mqttPort, WiFiClient espclient, const char* mqttUser,const char* mqttPassword){  
   client.setServer(mqttServer,mqttPort);
@@ -21,7 +57,7 @@ void mqttconnect(const char* mqttServer,const int mqttPort, WiFiClient espclient
           const char* message = "Hello World i am connected to MQTT";
           int length = strlen(message);
           boolean retained = true;
-          client.publish("prueba2",(byte*)message,length,retained);
+          client.publish("oscarmelo/prueba2",(byte*)message,length,retained);
           Serial.println("mensaje sent");
           if(client.subscribe("prueba1"))
               Serial.println("Suscribed to topic!");
@@ -40,8 +76,11 @@ void mqttconnect(const char* mqttServer,const int mqttPort, WiFiClient espclient
 
 void setup() {
   Serial.begin(115200);
-  pinMode(2,OUTPUT);
-  wifi.wificonect();
+  pinMode(2,INPUT);
+  pinMode(3,OUTPUT);
+  Serial.println("\nStarting AutoConnectAP");
+  autoconnectap();
+  // wifi.wificonect();
   mqttconnect("broker.mqttdashboard.com",1883, espclient,"racso","bimborico22D");
   client.setCallback(callback);
 }
@@ -49,13 +88,14 @@ void setup() {
 void loop() {
   if(activar == 49){
     Serial.println("Alarma activada");
-    digitalWrite(2,HIGH);
+    digitalWrite(3,HIGH);
     activar = 50;
   }
   if(activar == 48){
     Serial.println("Alarma desactivada");
-    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
     activar = 50;
   }
   client.loop();
+  wifi.check_status();
 }
